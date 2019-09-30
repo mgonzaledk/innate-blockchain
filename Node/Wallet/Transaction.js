e17b03f77173556bd324cab83ecc65e71b12f574
mgonzaledk <mgonzaledk@alumnos.unex.es>
2019-09-30 23:55:01 +0200

    Transacciones con datos de entrada y salida

--- Node/Wallet/Transaction.js
+++ Node/Wallet/Transaction.js
@@ -8,6 +8,10 @@ class Transaction {
             recipient,
             amount
         })
+        this.input =  this.createInput({
+            senderWallet,
+            outputMap: this.outputMap
+        })
     }
 
     createOutputMap({ senderWallet, recipient, amount }) {
@@ -17,6 +21,15 @@ class Transaction {
         outputMap[senderWallet.publicKey] = senderWallet.balance - amount
 
         return outputMap
+    }
+
+    createInput({ senderWallet, outputMap }) {
+        return {
+            timestamp: Date.now(),
+            amount: senderWallet.balance,
+            address: senderWallet.publicKey,
+            signature: senderWallet.sign(outputMap)
+        }
     }
 }
 
--- Node/Wallet/Transaction.test.js
+++ Node/Wallet/Transaction.test.js
@@ -1,5 +1,6 @@
 const Wallet = require('./index')
 const Transaction = require('./Transaction')
+const { verifySignature } = require('../Util')
 
 describe('Transaction', () => {
     let transaction, senderWallet, recipient, amount
@@ -32,6 +33,32 @@ describe('Transaction', () => {
         it('genera la salida restante (balance) de vuelta a la cartera de envío `senderWaller`', () => {
             expect(transaction.outputMap[senderWallet.publicKey])
                 .toEqual(senderWallet.balance - amount)
+        })
+    })
+
+    describe('input', () => {
+        it('tiene una entrada `input`', () => {
+            expect(transaction).toHaveProperty('input')
+        })
+
+        it('tiene una marca de tiempo `timestamp` para la entrada', () => {
+            expect(transaction.input).toHaveProperty('timestamp')
+        })
+
+        it('establece la cantidad `amount` del balance de la cartera `senderWallet`', () => {
+            expect(transaction.input.amount).toEqual(senderWallet.balance)
+        })
+
+        it('establece la dirección `address` como la clave pública de la cartera', () => {
+            expect(transaction.input.address).toEqual(senderWallet.publicKey)
+        })
+
+        it('firma su contenido', () => {
+            expect(verifySignature({
+                publicKey: senderWallet.publicKey,
+                data: transaction.outputMap,
+                signature: transaction.input.signature
+            })).toBe(true)
         })
     })
 })
