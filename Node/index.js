28c17a0daa08c9a6b74c46d14885dee3bc86ba63
mgonzaledk <mgonzaledk@alumnos.unex.es>
2019-10-01 11:30:23 +0200

    API con TransactionPool y gestión de errores

--- Node/index.js
+++ Node/index.js
@@ -5,10 +5,14 @@ const request = require('request')
 const Blockchain = require('./Blockchain')
 const { PORT, ROOT_NODE_ADDRESS } = require('./Config')
 const PubSub = require('./Server/PubSub')
+const TransactionPool = require('./Wallet/TransactionPool')
+const Wallet = require('./Wallet')
 
 const app = express()
 const blockchain = new Blockchain()
 const pubsub = new PubSub({ blockchain })
+const transactionPool = new TransactionPool()
+const wallet = new Wallet()
 
 app.use(bodyParser.json())
 
@@ -23,6 +27,21 @@ app.post('/v1/mine', (req, res) => {
     pubsub.broadcastChain()
 
     res.redirect('/v1/blocks')
+})
+
+app.post('/v1/transaction', (req, res) => {
+    const { amount, recipient } = req.body
+    let transaction
+
+    try {
+        transaction = wallet.createTransaction({ amount, recipient })
+    } catch(error) {
+        return res.status(400).json({ type: 'error', message: error.message })
+    }
+
+    transactionPool.add(transaction)
+
+    res.json({ type: 'success', transaction })
 })
 
 const syncChains = () => {
