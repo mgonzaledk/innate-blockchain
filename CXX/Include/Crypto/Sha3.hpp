ab020cc5bd5e447a3d7e7192b36f8d2906cd890b
mgonzaledk <mgonzaledk@alumnos.unex.es>
2019-10-20 19:46:12 +0200

    Sha3 256/384/512

--- CXX/Include/Crypto/Sha3.hpp
+++ CXX/Include/Crypto/Sha3.hpp
@@ -14,8 +14,12 @@
 class Sha3 {
 public:
     typedef std::array<std::byte, 32> Sha3_256_Result;
+    typedef std::array<std::byte, 48> Sha3_384_Result;
+    typedef std::array<std::byte, 64> Sha3_512_Result;
 
     static Sha3_256_Result Sha3_256(const std::string &string);
+    static Sha3_384_Result Sha3_384(const std::string &string);
+    static Sha3_512_Result Sha3_512(const std::string &string);
 };
 
 #endif //INNATE_BLOCKCHAIN_CXX_SHA3_H
--- CXX/Source/Crypto/Sha3.cpp
+++ CXX/Source/Crypto/Sha3.cpp
@@ -17,3 +17,31 @@ Sha3::Sha3_256_Result Sha3::Sha3_256(const std::string &string) {
     std::move(std::begin(bytes), std::end(bytes), result.begin());
     return result;
 }
+
+Sha3::Sha3_384_Result Sha3::Sha3_384(const std::string &string) {
+    Sha3::Sha3_384_Result result;
+    std::byte bytes[48];
+
+    sha3_384(
+            (const unsigned char *)string.c_str(),
+            string.length(),
+            (unsigned char *)bytes
+    );
+
+    std::move(std::begin(bytes), std::end(bytes), result.begin());
+    return result;
+}
+
+Sha3::Sha3_512_Result Sha3::Sha3_512(const std::string &string) {
+    Sha3::Sha3_512_Result result;
+    std::byte bytes[64];
+
+    sha3_512(
+            (const unsigned char *)string.c_str(),
+            string.length(),
+            (unsigned char *)bytes
+    );
+
+    std::move(std::begin(bytes), std::end(bytes), result.begin());
+    return result;
+}
